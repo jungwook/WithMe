23de66b23daeb52c37216302954e819756614810
한정욱 <jungwook.hahn@gmail.com>
2016-08-01 13:51:40 +0900

    some animations

--- WithMe/AdCell.m
+++ WithMe/AdCell.m
@@ -8,6 +8,7 @@
 
 #import "AdCell.h"
 @interface AdCell()
+@property (weak, nonatomic) IBOutlet UIView *canvas;
 @property (weak, nonatomic) IBOutlet UILabel *title;
 @property (weak, nonatomic) IBOutlet UIView *bottomBox;
 @end
@@ -46,6 +47,48 @@
     self.bottomBox.layer.shadowPath = [UIBezierPath bezierPathWithRect:self.bottomBox.bounds].CGPath;
 }
 
+-(void) clearCanvas
+{
+    [self.canvas.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [obj removeFromSuperview];
+    }];
+}
+
+- (CAAnimation*) labelAnimation
+{
+
+    CABasicAnimation *ta1 =[CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    ta1.duration = 1;
+    ta1.beginTime = arc4random()%100;
+    ta1.repeatCount = INFINITY;
+    ta1.autoreverses = NO;
+    ta1.fromValue = @(0);
+    ta1.toValue = @(10);
+    ta1.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    
+    
+    
+    
+    return ta1;
+}
+
+- (void) fillCanvasWith:(NSArray*)subCategories
+{
+    [subCategories enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UILabel *label = [UILabel new];
+        label.text = obj;
+        label.font = appFont(14);
+        label.frame = rectForString(obj, label.font, INFINITY);
+        
+        CGFloat w = label.bounds.size.width, h = label.bounds.size.height, W = self.bounds.size.width, H = self.bounds.size.height;
+        CGFloat x = arc4random() % MAX((NSInteger) (W-w),1);
+        CGFloat y = arc4random() % MAX((NSInteger) (H-h),1);
+        
+        label.frame = CGRectMake(x, y, w, h);
+        [label.layer addAnimation:[self labelAnimation] forKey:nil];
+        [self.canvas addSubview:label];
+    }];
+}
 
 - (void)setActivity:(id)activity
 {
@@ -58,19 +101,34 @@
         self.title.textColor = colorBlue;
         self.title.shadowColor = [colorWhite colorWithAlphaComponent:0.8];
         self.title.shadowOffset = CGSizeMake(0, -1);
-//        self.backgroundView.backgroundColor = colorWhite;
     }
     else if ([activity isKindOfClass:[NSDictionary class]]){
         self.title.text = activity[@"title"];
         self.title.textColor = [UIColor colorWithWhite:0.2 alpha:1.0];
         self.title.shadowColor = [colorWhite colorWithAlphaComponent:0.8];
         self.title.shadowOffset = CGSizeMake(0, 1);
-//        self.backgroundView.backgroundColor = colorWhite;
+        [self clearCanvas];
+        [self fillCanvasWith:[self subCategories:activity[@"content"]]];
     }
     else {
         self.backgroundView.backgroundColor = [UIColor redColor];
         self.title.text = @"UNKNOWN";
     }
+}
+
+- (NSArray*) subCategories:(NSArray*)ad
+{
+    NSMutableArray *ret = [NSMutableArray array];
+    [ad enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:[NSString class]]) {
+            [ret addObject:obj];
+        }
+        else {
+            id title = [obj objectForKey:@"title"];
+            [ret addObject:title];
+        }
+    }];
+    return ret;
 }
 
 @end
