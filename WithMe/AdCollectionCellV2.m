07209fc75b03566a0816316d1d66b163d803a78f
한정욱 <jungwook.hahn@gmail.com>
2016-08-31 15:27:20 +0900

    ok

--- WithMe/AdCollectionCellV2.m
+++ WithMe/AdCollectionCellV2.m
@@ -8,14 +8,50 @@
 
 #import "AdCollectionCellV2.h"
 
+@interface AdCollectionCellV2()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nicknameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *likedByLabel;
+@property (weak, nonatomic) IBOutlet UILabel *viewedByLabel;
+@property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
+@property (weak, nonatomic) IBOutlet UILabel *activityLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *likedByIcon;
+@property (weak, nonatomic) IBOutlet UIImageView *viewedByIcon;
+
+@end
+
 @implementation AdCollectionCellV2
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)setAd:(Ad *)ad
+{
+    super.ad = ad;
+    self.imageView.image = nil;
+    
+    self.viewedByIcon.tintColor = kAppColor;
+    self.likedByIcon.tintColor = kAppColor;
+    
+    self.categoryLabel.backgroundColor = kAppColor;
+    self.activityLabel.backgroundColor = kAppColor;
+    self.titleLabel.textColor = kAppColor.darkerColor;
+    
+    [self.ad fetched:^{
+        self.categoryLabel.text = ad.activity.category.name;
+        self.activityLabel.text = ad.activity.name;
+        self.titleLabel.text = ad.title;
+        
+        [self.ad firstThumbnailImageLoaded:^(UIImage *image) {
+            if (ad == self.ad) {
+                self.imageView.image = image;
+            }
+        }];
+    }];
 }
-*/
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    [self.layer addAnimation:buttonPressedAnimation() forKey:nil];
+}
 
 @end
